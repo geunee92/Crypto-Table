192343f7d8606ca9e4b5d544eb8553bda9c0476a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-03 02:41:39 +0000

    Deploy writingdeveloper/Crypto-Table to github.com/writingdeveloper/Crypto-Table.git:gh-pages

---
+++ precache-manifest.4de711c892c2c3276f10b07a941d5155.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca893dfa59e41b4ffaedeef38e94a12b",
+    "url": "/Crypto-Table/index.html"
+  },
+  {
+    "revision": "f6197963f586aec75665",
+    "url": "/Crypto-Table/static/css/main.5596ae68.chunk.css"
+  },
+  {
+    "revision": "1b5eafcb404f21d509f9",
+    "url": "/Crypto-Table/static/js/2.24848ae7.chunk.js"
+  },
+  {
+    "revision": "fc41eec88cc1adaf9c952d6014fa83cd",
+    "url": "/Crypto-Table/static/js/2.24848ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6197963f586aec75665",
+    "url": "/Crypto-Table/static/js/main.368eb25e.chunk.js"
+  },
+  {
+    "revision": "554c98646cc06a7dc329",
+    "url": "/Crypto-Table/static/js/runtime-main.2dc5e511.js"
+  }
+]);
